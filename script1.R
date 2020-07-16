021514469e0a163811a84cc03373ccbd5f19f7ed
Marcus Suassuna <msuassuna@gmail.com>
2020-07-15 22:44:01 -0300

    add new R script

---
+++ script1.R
@@ -0,0 +1,12 @@
+library(fivethirtyeight)
+library(tidyverse)
+
+data("steak_survey", package="fivethirtyeight")
+
+sdat <- na.omit(steak_survey) %>%
+  filter(region=="Mountain")
+table(steak_survey$region)
+ggplot(sdat, 
+       aes(x = steak_prep, fill = female)) + 
+  geom_bar(position="dodge") +
+  ggtitle(paste0("Steak Preparation Preference by Gender for ","Mountain"," Region"))
