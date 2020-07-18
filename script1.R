08557d3f746687b76d3f9ba89eae20d8a9258a35
Marcus Suassuna <msuassuna@gmail.com>
2020-07-17 22:47:57 -0300

    updates to the Stake Article document

--- script1.R
+++ script1.R
@@ -5,7 +5,9 @@ data("steak_survey", package="fivethirtyeight")
 
 sdat <- na.omit(steak_survey) %>%
   filter(region=="Mountain")
+
 table(steak_survey$region)
+
 ggplot(sdat, 
        aes(x = steak_prep, fill = female)) + 
   geom_bar(position="dodge") +
